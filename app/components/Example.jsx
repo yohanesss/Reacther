94dca11ee3f039020b4835b141a5fe1ffb6050b1
ShuXian <test27c@gmail.com>
2017-04-11 08:51:33 +0700

    Add style to example page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,7 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 var Example = (render) => {
     return (
-      <h3>Example</h3>
+      <div>        
+        <h3 className="text-center">Example</h3>
+        <p>Here are a few example location to try out:</p>
+        <ol>
+          <li>
+            <Link to="/?location=Jakarta">Jakarta, Indonesia</Link>
+          </li>
+          <li>
+            <Link to="/?location=Bandung">Tokyo, Japan</Link>
+          </li>
+        </ol>
+      </div>
     );
   };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,7 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      {props.children}
+        <div className="row">
+          <div className="column medium-6 large-4 small-centered">
+            {props.children}
+          </div>
+        </div>
     </div>
   )
 };
