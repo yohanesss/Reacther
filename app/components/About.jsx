15899a31fb62c6c8d256cb899b815d7872d00584
ShuXian <test27c@gmail.com>
2017-04-11 08:42:16 +0700

    add style to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var About = (render) => {
   return(
-    <h3>About</h3>
+    <div className="text-center">
+      <h1>About</h1>
+      <p>Reacther is a weather application built on React.</p>
+      <p>Using <a href="http://openweathermap.org">Open Weather Map</a> api.</p>
+    </div>
   );
 };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,7 +5,6 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
       {props.children}
     </div>
   )
